a7ec067ee60581c38b29d813fc9b411e7b2a0cd1
may <ykmshiss@gmail.com>
2023-01-25 02:47:23 +0900

    no message

--- laravel-study/resources/views/photos/create.blade.php
+++ laravel-study/resources/views/photos/create.blade.php
@@ -6,7 +6,7 @@
     @if(session()->has('success'))
       <p>{{session('success')}}</p>
     @endif
-    <form action="{{route('photos.store')}}" method="POST" enctype="mvltipart/form-data">
+    <form action="{{route('photos.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         <label><input type="file" name="image"></label>
         <button type="submit">アップロード</button>
