212bf18fc5f9de00fe89def0c4c23b0ad3041cd7
may <ykmshiss@gmail.com>
2023-01-31 17:39:03 +0900

    no message

---
+++ cat-cafe/app/Http/Controllers/ContactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContactRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact.index');
+    }
+
+    public function sendMail(ContactRequest $request)
+    {
+        $validated = $request->validated();
+
+
+        Log::debug($validated['name']. 'さんよりお問い合わせがありました');
+
+        return to_route('contact.complete');
+
+    }
+
+    public function complete()
+    {
+        return view('contact.complete');
+    }
+}
+
